33225aaa41d3cba8b8726be6683a08a479fc2ad4
wyf <wyf@gmail.com>
2017-12-09 08:50:33 +0800

    增加了首页 给首页增加了echarts插件

---
+++ public/manage_my/js/index.js
@@ -0,0 +1,72 @@
+$(function () {
+    // 导入第一个echarts 柱状图
+    // 基于准备好的dom，初始化echarts实例
+    var myChart1 = echarts.init(document.getElementById('main1'));
+
+    // 指定图表的配置项和数据
+    var option1 = {
+        title: {
+            text: '2017年注册人数'
+        },
+        tooltip: {},
+        legend: {
+            data: ['人数']
+        },
+        xAxis: {
+            data: ["6月", "7月", "8月", "9月", "10月", "11月"]
+        },
+        yAxis: {},
+        series: [{
+            name: '销量',
+            type: 'bar',
+            data: [500, 2000, 3600, 1000, 1000, 2000]
+        }]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart1.setOption(option1);
+
+    var myChart2 = echarts.init(document.getElementById('main2'));
+    
+    // 导入第二个饼状图
+    option2 = {
+        title : {
+            text: '热门品牌销售',
+            subtext: '2017年11月',
+            x:'center'
+        },
+        tooltip : {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left',
+            data: ['耐克','阿迪','新百伦','安踏','李宁']
+        },
+        series : [
+            {
+                name: '访问来源',
+                type: 'pie',
+                radius : '55%',
+                center: ['50%', '60%'],
+                data:[
+                    {value:335, name:'耐克'},
+                    {value:310, name:'阿迪'},
+                    {value:234, name:'新百伦'},
+                    {value:135, name:'安踏'},
+                    {value:1548, name:'李宁'}
+                ],
+                itemStyle: {
+                    emphasis: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    };
+    // 使用刚指定的配置项和数据显示图表。
+    myChart2.setOption(option2);
+})
