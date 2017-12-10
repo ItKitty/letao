5843ec5fcb741a75cc1608e5730fba85abd8a3b3
wyf <wyf@gmail.com>
2017-12-10 15:48:29 +0800

    完善了二级分类

--- public/manage_my/js/second.js
+++ public/manage_my/js/second.js
@@ -31,8 +31,51 @@ $(function () {
     }
     getDate();
 
+    // 上传图片
+    $("#fileUpload").fileupload({
+        dataType: "json",
+        //e：事件对象
+        //data：图片上传后的对象，通过e.result.picAddr可以获取上传后的图片地址
+        done: function (e, data) {
+            console.log(data);
+            $('form img').attr('src', data.result.picAddr);
+            // 将地址路径赋值给input
+            $('input[name=brandLogo]').val(data.result.picAddr);
+            // 如果动态改变 
+            $("form").data('bootstrapValidator').updateStatus('brandLogo', 'VALID');
+        }
+    });
+
+    // 点击ul改变
+    $.ajax({
+        url: "/category/queryTopCategoryPaging",
+        data: {
+            page: 1,
+            pageSize: 256
+        },
+        success: function (backData) {
+            $('.dropdown-menu').html("");
+            $.each(backData.rows, function (i, n) {
+                var li = $("<li><a data-id='" + n.id + "' href='javascript:;'>" + n.categoryName + "</a></li>");
+                $('.dropdown-menu').append(li);
+            })
+        }
+    })
+
+    $('.dropdown-menu').on('click', 'a', function () {
+        var value = $(this).html();
+        // console.log(value);
+        $('.select-value').html(value);
+        // 将ID赋值给input的value；
+        $('input[name=categoryId]').val($(this).data('id'));
+        // 如果动态改变 
+        $("form").data('bootstrapValidator').updateStatus('categoryId', 'VALID');
+    })
+
     // 表单数据验证
     $('form').bootstrapValidator({
+        //1. 指定不校验的类型，默认为[':disabled', ':hidden', ':not(:visible)'],可以不设置
+        excluded: [':disabled'],
         feedbackIcons: {
             valid: 'glyphicon glyphicon-ok',
             invalid: 'glyphicon glyphicon-heart',
@@ -40,12 +83,30 @@ $(function () {
         },
         // 检验的字段
         fields: {
-            categoryName: {
+            categoryId: {
                 // 验证什么
                 validators: {
                     // 非空
                     notEmpty: {
-                        message: "分类不能为空"
+                        message: "分类选择不能为空"
+                    }
+                }
+            },
+            brandName: {
+                // 验证什么
+                validators: {
+                    // 非空
+                    notEmpty: {
+                        message: "分类名称不能为空"
+                    }
+                }
+            },
+            brandLogo: {
+                // 验证什么
+                validators: {
+                    // 非空
+                    notEmpty: {
+                        message: "品牌LOGO不能为空"
                     }
                 }
             }
@@ -54,7 +115,7 @@ $(function () {
         e.preventDefault();
         //使用ajax提交逻辑
         $.ajax({
-            url: "/category/addTopCategory",
+            url: "/category/addSecondCategory",
             data: $('form').serialize(),
             type: 'post',
             success: function (backData) {
@@ -64,40 +125,14 @@ $(function () {
                 // 重新获取当前页数据
                 getDate();
 
+                $("form").data('bootstrapValidator').resetForm()
+                $('.select-value').html('请选择');
+                $('input').val("");
+                $('form img').attr('src', './images/none.png');
+
             }
         })
+
     });
 
-    // 上传图片
-    $("#fileUpload").fileupload({
-        dataType: "json",
-        //e：事件对象
-        //data：图片上传后的对象，通过e.result.picAddr可以获取上传后的图片地址
-        done: function (e, data) {
-            console.log(data);
-            $('form img').attr('src',data.result.picAddr);
-        }
-    });
-
-    // 点击ul改变
-    $.ajax({
-        url:"/category/queryTopCategoryPaging",
-        data:{
-            page:1,
-            pageSize:256
-        },
-        success:function(backData){
-            $('.dropdown-menu').html("");
-            $.each(backData.rows,function(i,n){
-                var li =$("<li><a href='javascript:;'>"+n.categoryName+"</a></li>");
-                $('.dropdown-menu').append(li);
-            })
-        }
-    })
-
-    $('.dropdown-menu').on('click','a',function(){
-        var value = $(this).html();
-        console.log(value);
-        $('.select-value').html(value);
-    })
 })
