3f7737b6fd4e612046b00f405e866dc44e796cd9
wyf <wyf@gmail.com>
2017-12-11 11:11:51 +0800

    勉强算是吧产品做完了

--- public/manage_my/js/products.js
+++ public/manage_my/js/products.js
@@ -34,28 +34,28 @@ $(function () {
     $('tbody').on('click', 'button:eq(0)', function () {
         // console.log("1");
         var myStatu = $(this).html() == '下架' ? 1 : 0;
-        var myId = $(this).parent().data('id');
-        var myBrandId = $(this).parent().data('brandid');
-        var proName = $(this).parent().siblings('td').data('proname');
-        var oldPrice = $(this).parent().siblings('td').data('oldprice');
-        var price = $(this).parent().siblings('td').data('price');
-        var proDesc = $(this).parent().siblings('td').data('prodesc');
-        var size = $(this).parent().siblings('td').data('size');
-        var num = $(this).parent().siblings('td').data('num');
+        // var myId = $(this).parent().data('id');
+        // var myBrandId = $(this).parent().data('brandid');
+        // var proName = $(this).parent().siblings('td').data('proname');
+        // var oldPrice = $(this).parent().siblings('td').data('oldprice');
+        // var price = $(this).parent().siblings('td').data('price');
+        // var proDesc = $(this).parent().siblings('td').data('prodesc');
+        // var size = $(this).parent().siblings('td').data('size');
+        // var num = $(this).parent().siblings('td').data('num');
 
-        console.log(myStatu + "|" + myId + "|" + myBrandId + "|" + proName + "|" + oldPrice + "|" + price + "|" + proDesc + "|" + size + "|" + num)
+        // console.log(myStatu + "|" + myId + "|" + myBrandId + "|" + proName + "|" + oldPrice + "|" + price + "|" + proDesc + "|" + size + "|" + num)
 
         $.ajax({
             url: "/product/updateProduct",
             data: {
-                id: myId,
-                proName: proName,
-                oldPrice: oldPrice,
-                price: price,
-                proDesc: proDesc,
-                size: size,
-                num: num,
-                brandId: myBrandId,
+                // id: myId,
+                // proName: proName,
+                // oldPrice: oldPrice,
+                // price: price,
+                // proDesc: proDesc,
+                // size: size,
+                // num: num,
+                // brandId: myBrandId,
                 statu: myStatu
             },
             type: 'post',
@@ -65,5 +65,135 @@ $(function () {
             }
         })
     })
+
+    // 图片上传验证
+    $("#fileUpload").fileupload({
+        dataType: "json",
+        //e：事件对象
+        //data：图片上传后的对象，通过e.result.picAddr可以获取上传后的图片地址
+        done: function (e, data) {
+            $('<img width="100" height="100" style="margin-top:10px;margin-right:10px" src="' + data.result.picAddr + '" alt="">').appendTo($('.fileupload'));
+
+            // 人为动态更新字段
+            if ($('.fileupload img').length == 3) {
+                $('form').data('bootstrapValidator').updateStatus('pic1', 'VALID');
+            }
+        }
+    });
+
+    // 当图片超过3张的时候不能添加
+    $('#fileUpload').on('click', function (e) {
+        var num = $(this).siblings('img').length;
+        if (num == 3) {
+            e.preventDefault();
+        }
+    })
+
+    // 双击图片 图片删除
+    $('.fileupload').on('dblclick', 'img', function () {
+        console.log("1");
+        $(this).remove();
+    })
+
+    // 表单数据验证
+    $('form').bootstrapValidator({
+        //1. 指定不校验的类型，默认为[':disabled', ':hidden', ':not(:visible)'],可以不设置
+        // excluded: [':disabled'],
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-heart',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        // 检验的字段
+        fields: {
+            proName: {
+                // 验证什么
+                validators: {
+                    // 非空
+                    notEmpty: {
+                        message: "商品名称不能为空"
+                    }
+                }
+            },
+            proDesc: {
+                // 验证什么
+                validators: {
+                    // 非空
+                    notEmpty: {
+                        message: "商品描述不能为空"
+                    }
+                }
+            },
+            num: {
+                // 验证什么
+                validators: {
+                    // 非空
+                    notEmpty: {
+                        message: "商品库存不能为空"
+                    }
+                }
+            },
+            price: {
+                // 验证什么
+                validators: {
+                    // 非空
+                    notEmpty: {
+                        message: "商品价格不能为空"
+                    }
+                }
+            },
+            oldPrice: {
+                // 验证什么
+                validators: {
+                    // 非空
+                    notEmpty: {
+                        message: "商品原价不能为空"
+                    }
+                }
+            },
+            size: {
+                // 验证什么
+                validators: {
+                    // 非空
+                    notEmpty: {
+                        message: "商品尺寸不能为空"
+                    }
+                }
+            },
+            pic1: {
+                // 验证什么
+                validators: {
+                    // 非空
+                    notEmpty: {
+                        message: "商品LOGO不能为空"
+                    }
+                }
+            }
+        }
+    }).on('success.form.bv', function (e) {
+        e.preventDefault();
+        // 判断是否传入3张
+        if($('.fileupload img').length<3){
+            alert("请输入3张图片")
+        };
+        //使用ajax提交逻辑
+        $.ajax({
+            url: "/product/addProduct",
+            data: $('form').serialize(),
+            type: 'post',
+            success: function (backData) {
+                console.log(backData);
+                // 关闭modal
+                $('.modal-add').modal('hide')
+                // 重新获取当前页数据
+                getDate();
+
+                $("form").data('bootstrapValidator').resetForm()
+                $('input').val("");
+
+            }
+        })
+
+    });
 
 })
