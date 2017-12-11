20a91f540709ad0a1c3d56a4f78cbe11f2b40f90
wyf <wyf@gmail.com>
2017-12-11 21:46:22 +0800

    移动端首页布局完成

--- public/mobile_my/js/common.js
+++ public/mobile_my/js/common.js
@@ -1,3 +1,32 @@
-$(function(){
-    
+$(function () {
+
+    // 当页面加载完成执行事件
+    window.addEventListener('load', function () {
+        windowResize();
+        muiFn();
+    })
+
+    // 运用rem 自适应界面
+    function windowResize() {
+        var w = document.body.offsetWidth;
+        var fz = w / 375 * 100;
+        document.querySelector('html').style.fontSize = fz + 'px';
+        window.addEventListener('resize', function () {
+            w = document.body.offsetWidth;
+            fz = w / 375 * 100;
+            document.querySelector('html').style.fontSize = fz + 'px';
+        })
+    }
+
+    function muiFn() {
+        // 滚动mui
+        mui('.mui-scroll-wrapper').scroll({
+            deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+        });
+        // 自动轮播
+        mui(".mui-slider").slider({
+            interval: 2000
+        });
+
+    }
 })
